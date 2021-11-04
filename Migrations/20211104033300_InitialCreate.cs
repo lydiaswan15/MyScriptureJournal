dd31f1048456c11489e63da1a8592530d6b1e8fa
lydiaswan15 <lydiaswan15@gmail.com>
2021-11-03 21:34:01 -0600

    Migration stuff

---
+++ Migrations/20211104033300_InitialCreate.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MySciptureJournal.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Book",
+                columns: table => new
+                {
+                    BookId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    BookName = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Book", x => x.BookId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Scripture",
+                columns: table => new
+                {
+                    ScriptureId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    BookId = table.Column<int>(type: "INTEGER", nullable: false),
+                    Verses = table.Column<string>(type: "TEXT", nullable: true),
+                    Notes = table.Column<string>(type: "TEXT", nullable: true),
+                    DateCreated = table.Column<DateTime>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Scripture", x => x.ScriptureId);
+                    table.ForeignKey(
+                        name: "FK_Scripture_Book_BookId",
+                        column: x => x.BookId,
+                        principalTable: "Book",
+                        principalColumn: "BookId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Scripture_BookId",
+                table: "Scripture",
+                column: "BookId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Scripture");
+
+            migrationBuilder.DropTable(
+                name: "Book");
+        }
+    }
+}
--- Startup.cs
+++ Startup.cs
@@ -14,33 +14,42 @@ namespace MySciptureJournal
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public Startup(IConfiguration configuration, IWebHostEnvironment env)
         {
+            Environment = env;
             Configuration = configuration;
         }
 
         public IConfiguration Configuration { get; }
+        public IWebHostEnvironment Environment { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            if (Environment.IsDevelopment())
+            {
+                services.AddDbContext<MyScriptureJournalContext>(options =>
+                options.UseSqlite(
+                    Configuration.GetConnectionString("MyScriptureJournalContext")));
+            }
+            else
+            {
+                services.AddDbContext<MyScriptureJournalContext>(options =>
+                options.UseSqlServer(
+                    Configuration.GetConnectionString("ScriptureContext")));
+            }
 
-            services.AddDbContext<MyScriptureJournalContext>(options =>
-                    options.UseSqlite(Configuration.GetConnectionString("MyScriptureJournalContext")));
+            services.AddRazorPages();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app)
         {
-            if (env.IsDevelopment())
+            if (Environment.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
             }
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -48,8 +57,6 @@ namespace MySciptureJournal
             app.UseStaticFiles();
 
             app.UseRouting();
-
-            app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
             {
